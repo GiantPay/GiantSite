4e73cdc992336142129ed291394e44e6e7698d84
Lollipop <lollipop@devadmin.com>
2018-11-08 15:06:37 +0600

    add new endpoint to calc controller

--- src/main/java/network/giantpay/web/CalcController.java
+++ src/main/java/network/giantpay/web/CalcController.java
@@ -1,13 +1,21 @@
 package network.giantpay.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class CalcController {
 
     @GetMapping("/calc")
-    public String calc() {
-        return "calc";
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    public String oldUrl() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/proof")
+    public String proofOfSake() {
+        return "proof";
     }
 }
