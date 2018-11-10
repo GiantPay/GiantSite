9e82fce03abb6317d8d0a0dda19475894bac416a
Lollipop <lollipop@devadmin.com>
2018-11-10 15:49:10 +0600

    movw scheduler from main to config

--- src/main/java/network/giantpay/GiantApplication.java
+++ src/main/java/network/giantpay/GiantApplication.java
@@ -2,9 +2,7 @@ package network.giantpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class GiantApplication {
 
--- src/main/java/network/giantpay/config/SchedulerConfig.java
+++ src/main/java/network/giantpay/config/SchedulerConfig.java
@@ -1,15 +1,21 @@
 package network.giantpay.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * we don't need Scheduling in dev environment
+ * We don't need Scheduling in dev environment
  */
 @Configuration
 @EnableScheduling
 @Profile("!dev")
-public  class SchedulerConfig {
+@Slf4j
+public class SchedulerConfig {
+
+    private void init(){
+        log.info("Scheduler is enabled");
+    }
 
 }
