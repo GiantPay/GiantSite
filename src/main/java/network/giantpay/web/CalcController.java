bab097929acbe6d29faf69303a4ab86dfa7b8109
Lollipop <lollipop@devadmin.com>
2018-11-10 15:47:30 +0600

    Fix calculation in Proof-of-State

--- src/main/java/network/giantpay/web/CalcController.java
+++ src/main/java/network/giantpay/web/CalcController.java
@@ -1,12 +1,21 @@
 package network.giantpay.web;
 
+import lombok.AllArgsConstructor;
+import network.giantpay.dto.RateDto;
+import network.giantpay.service.MonitoringService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.math.RoundingMode;
+import java.util.Map;
+
 @Controller
+@AllArgsConstructor
 public class CalcController {
+
+    private final MonitoringService monitoringService;
 
     @GetMapping("/calc")
     @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
@@ -15,7 +24,12 @@ public class CalcController {
     }
 
     @GetMapping("/proof")
-    public String proofOfSake() {
+    public String proofOfSake(final Map<String, Object> model) {
+        final RateDto rates = this.monitoringService.getRates();
+
+        model.put("btcRate", rates.getBtc().setScale(8, RoundingMode.HALF_DOWN));
+        model.put("usdRate", rates.getUsd().setScale(2, RoundingMode.HALF_DOWN));
+
         return "proof";
     }
 }
