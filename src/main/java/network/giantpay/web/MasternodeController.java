ec51c3d7f921303a9a7514855c43a8fc95b630e6
Lollipop <lollipop@devadmin.com>
2018-11-19 10:56:49 +0600

    change initial masternodes count value

--- src/main/java/network/giantpay/web/MasternodeController.java
+++ src/main/java/network/giantpay/web/MasternodeController.java
@@ -1,6 +1,7 @@
 package network.giantpay.web;
 
 import lombok.AllArgsConstructor;
+import network.giantpay.service.MonitoringService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,11 +14,13 @@ public class MasternodeController {
 
     private final Consumer<Map<String, Object>> rateService;
 
+    private final MonitoringService monitoringService;
+
     @GetMapping("/masternodes")
     public String index(final Map<String, Object> model) {
         this.rateService.accept(model);
 
-        model.put("masternodes", 12);
+        model.put("masternodes", this.monitoringService.getMasternodeInfo().getMasternodeCount());
 
         return "masternodes";
     }
