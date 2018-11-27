42bb13af52f649b29f49ceaf236d1bb9d4285dc8
Lollipop <lollipop@devadmin.com>
2018-11-27 13:25:28 +0600

    add 301 redirect for old urls

--- src/main/java/network/giantpay/web/WhitepaperController.java
+++ src/main/java/network/giantpay/web/WhitepaperController.java
@@ -1,7 +1,11 @@
 package network.giantpay.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WhitepaperController {
@@ -19,5 +23,20 @@ public class WhitepaperController {
     @GetMapping("/giant-exchange")
     public String giantExchangeWhitepaper() {
         return "whitepapers/giant-exchange";
+    }
+
+
+    @GetMapping("/whitepaper/contracts")
+    public String oldContactsUrl(final HttpServletRequest request) {
+        request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.MOVED_PERMANENTLY);
+
+        return "redirect:/giant-contracts";
+    }
+
+    @GetMapping("/whitepaper/exchange")
+    public String oldExchangeUrl(final HttpServletRequest request) {
+        request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.MOVED_PERMANENTLY);
+
+        return "redirect:/giant-exchange";
     }
 }
