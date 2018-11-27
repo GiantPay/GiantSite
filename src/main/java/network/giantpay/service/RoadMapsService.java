13f86f2f5992b0e71778356d7781b275862afc7b
Lollipop <lollipop@devadmin.com>
2018-11-28 00:16:52 +0600

    add roadmap info to controller

--- src/main/java/network/giantpay/service/RoadMapsService.java
+++ src/main/java/network/giantpay/service/RoadMapsService.java
@@ -1,28 +1,22 @@
 package network.giantpay.service;
 
 import lombok.AllArgsConstructor;
+import network.giantpay.dto.RoadMapDto;
 import network.giantpay.model.RoadMap;
 import network.giantpay.repository.AvailableYearsRepository;
 import network.giantpay.repository.RoadMapRepository;
-import network.giantpay.utils.DateUtils;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 import static java.util.stream.Collectors.toList;
+import static network.giantpay.dto.Quarter.*;
+import static network.giantpay.utils.DateUtils.interval;
 
 @Service
 @AllArgsConstructor
 public class RoadMapsService {
-
-    private final static String Q1 = "Q1";
-
-    private final static String Q2 = "Q2";
-
-    private final static String Q3 = "Q3";
-
-    private final static String Q4 = "Q4";
 
     private final AvailableYearsRepository availableYearsRepository;
 
@@ -49,27 +43,59 @@ public class RoadMapsService {
      */
     public Map<Integer, Map<String, List<RoadMap>>> roadMapsByYear() {
         final List<Integer> years = this.availableYearsRepository.get();
-        final Map<Integer, Map<String, List<RoadMap>>> result = new HashMap<>(4);
-        final List<RoadMap> all = this.roadMapRepository.findAll();
+        final List<RoadMap> roadMaps = this.roadMapRepository.findAll();
+
+        final Map<Integer, Map<String, List<RoadMap>>> response = new HashMap<>(4);
 
         for (final int year : years) {
             final Map<String, List<RoadMap>> quarters = new HashMap<>(10);
 
-            final Pair<Date, Date> q1Interval = DateUtils.interval(year, Calendar.JANUARY, Calendar.MARCH);
-            quarters.put(Q1, filterByInterval(all, q1Interval));
+            final Pair<Date, Date> q1Interval = interval(year, Calendar.JANUARY, Calendar.MARCH);
+            quarters.put(Q1.value(), filterByInterval(roadMaps, q1Interval));
 
-            final Pair<Date, Date> q2Interval = DateUtils.interval(year, Calendar.APRIL, Calendar.JUNE);
-            quarters.put(Q2, filterByInterval(all, q2Interval));
+            final Pair<Date, Date> q2Interval = interval(year, Calendar.APRIL, Calendar.JUNE);
+            quarters.put(Q2.value(), filterByInterval(roadMaps, q2Interval));
 
-            final Pair<Date, Date> q3Interval = DateUtils.interval(year, Calendar.JULY, Calendar.SEPTEMBER);
-            quarters.put(Q3, filterByInterval(all, q3Interval));
+            final Pair<Date, Date> q3Interval = interval(year, Calendar.JULY, Calendar.SEPTEMBER);
+            quarters.put(Q3.value(), filterByInterval(roadMaps, q3Interval));
 
-            final Pair<Date, Date> q4Interval = DateUtils.interval(year, Calendar.OCTOBER, Calendar.DECEMBER);
-            quarters.put(Q4, filterByInterval(all, q4Interval));
+            final Pair<Date, Date> q4Interval = interval(year, Calendar.OCTOBER, Calendar.DECEMBER);
+            quarters.put(Q4.value(), filterByInterval(roadMaps, q4Interval));
 
-            result.put(year, quarters);
+            response.put(year, quarters);
         }
-        return result;
+        return response;
+    }
+    //// TODO: 11/28/18 rewrite this logic
+    public List<RoadMapDto> quarteredRoadMaps() {
+        final List<Integer> years = this.availableYearsRepository.get();
+        final List<RoadMap> roadMaps = this.roadMapRepository.findAll();
+
+        final List<RoadMapDto> response = new ArrayList<>(4);
+
+        for (final int year : years) {
+            final List<RoadMap> q1RoadMaps = filterByInterval(roadMaps, interval(year, Calendar.JANUARY, Calendar.MARCH));
+
+            final List<RoadMap> q2RoadMaps = filterByInterval(roadMaps, interval(year, Calendar.APRIL, Calendar.JUNE));
+
+            final List<RoadMap> q3RoadMaps = filterByInterval(roadMaps, interval(year, Calendar.JULY, Calendar.SEPTEMBER));
+
+            final List<RoadMap> q4RoadMaps = filterByInterval(roadMaps, interval(year, Calendar.OCTOBER, Calendar.DECEMBER));
+
+            if (!q1RoadMaps.isEmpty()) {
+                response.add(new RoadMapDto(year, Q1, q1RoadMaps));
+            }
+            if (!q2RoadMaps.isEmpty()) {
+                response.add(new RoadMapDto(year, Q2, q2RoadMaps));
+            }
+            if (!q3RoadMaps.isEmpty()) {
+                response.add(new RoadMapDto(year, Q3, q3RoadMaps));
+            }
+            if (!q4RoadMaps.isEmpty()) {
+                response.add(new RoadMapDto(year, Q4, q4RoadMaps));
+            }
+        }
+        return response;
     }
 
     /**
