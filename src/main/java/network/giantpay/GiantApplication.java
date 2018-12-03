2da7adbbaceb8a47ab236e976d1a50f81e91493c
Lollipop <lollipop@devadmin.com>
2018-12-03 13:57:12 +0600

    remove Command line runner

--- src/main/java/network/giantpay/GiantApplication.java
+++ src/main/java/network/giantpay/GiantApplication.java
@@ -1,16 +1,12 @@
 package network.giantpay;
 
-import com.redfin.sitemapgenerator.WebSitemapGenerator;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.File;
-
 @SpringBootApplication
 @EnableScheduling
-public class GiantApplication implements CommandLineRunner {
+public class GiantApplication {
 
     public static final String BASE_URL = "https://giantpay.network";
 
@@ -18,17 +14,5 @@ public class GiantApplication implements CommandLineRunner {
         SpringApplication.run(GiantApplication.class, args);
     }
 
-    @Override
-    public void run(final String... args) throws Exception {
-       /* try{
-            WebSitemapGenerator sitemapGenerator = WebSitemapGenerator.builder("https://giantpay.network/", new File(("/home/strogiyotec/site")))
-                    .build();
-            sitemapGenerator.addUrl("https://giantpay.network/team");
-            sitemapGenerator.write();
-        }catch (final Exception exc){
-            exc.printStackTrace();
-        }*/
-
-    }
 
 }
