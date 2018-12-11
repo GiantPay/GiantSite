107d6f031ee060c11b2be982c96656e1228b92ee
Lollipop <lollipop@devadmin.com>
2018-12-11 13:45:25 +0600

    move js to single file, remove unused scripts

--- src/main/java/network/giantpay/web/WhitepaperController.java
+++ src/main/java/network/giantpay/web/WhitepaperController.java
@@ -1,27 +1,28 @@
 package network.giantpay.web;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.View;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class WhitepaperController {
 
     @GetMapping("/whitepaper")
-    public String giantWhitepaper() {
+    public String giantWhitepaper(final Map<String, Object> model) {
+        model.put("giant",true);
         return "whitepapers/giant";
     }
 
     @GetMapping("/whitepaper/contracts")
-    public String giantContractsWhitepaper() {
+    public String giantContractsWhitepaper(final Map<String, Object> model) {
+        model.put("contracts",true);
         return "whitepapers/giant-contracts";
     }
 
     @GetMapping("/whitepaper/exchange")
-    public String giantExchangeWhitepaper() {
+    public String giantExchangeWhitepaper(final Map<String, Object> model) {
+        model.put("exchange",true);
         return "whitepapers/giant-exchange";
     }
 
