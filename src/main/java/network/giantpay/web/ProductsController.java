79072d690562e772909db6e58683250e17709271
Lollipop <lollipop@devadmin.com>
2018-12-11 14:32:10 +0600

    change product to boolean in model, remove unused js

--- src/main/java/network/giantpay/web/ProductsController.java
+++ src/main/java/network/giantpay/web/ProductsController.java
@@ -1,22 +1,30 @@
 package network.giantpay.web;
 
-import lombok.AllArgsConstructor;
-import network.giantpay.service.ActiveProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor
 public class ProductsController {
-
-    private final ActiveProductService activeProductService;
 
     @GetMapping("/products")
     public String products(@RequestParam(value = "page", required = false) final String page, final Map<String, Object> model) {
-        this.activeProductService.accept(page, model);
+        setActiveProduct(page, model);
         return "products";
+    }
+
+    private static void setActiveProduct(final String page, final Map<String, Object> model) {
+        if (!StringUtils.isEmpty(page)) {
+            if (page.equals("bet")) {
+                model.put("bet", true);
+            } else if (page.equals("coldstake")) {
+                model.put("coldstake", true);
+            }
+        } else {
+            model.put("exchange", true);
+        }
     }
 }
