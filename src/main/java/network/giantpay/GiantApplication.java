507f0cf3394548c91fbf517e989b3e7c4e3c169d
Lollipop <lollipop@devadmin.com>
2018-12-13 00:37:02 +0600

    add update war to jar

--- src/main/java/network/giantpay/GiantApplication.java
+++ src/main/java/network/giantpay/GiantApplication.java
@@ -2,15 +2,22 @@ package network.giantpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class GiantApplication {
+public class GiantApplication extends SpringBootServletInitializer{
 
     public static final String BASE_URL = "https://giantpay.network";
 
     public static void main(String[] args) {
         SpringApplication.run(GiantApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GiantApplication.class);
     }
 }
